6ebf4fc74f7c89770d4bd5d02b5b9ef8a5a28a2e
Moraes-Bruno <moraesbruno888@gmail.com>
2023-07-03 15:34:09 -0300

    number guessing game

---
+++ number_guessing_game/index.js
@@ -0,0 +1,20 @@
+const ANSWEAR = Math.floor(Math.random() * 10 + 1);
+let guesses = 0;
+
+document.getElementById("botao").onclick = function () {
+
+    let guess = document.getElementById("num").value
+    guesses++;
+
+    if (guess == ANSWEAR) {
+        window.alert(`${ANSWEAR} is the number.It took you ${guesses} guesses`);
+    } else if (guess < ANSWEAR) {
+        window.alert("Too small")
+    } else {
+        window.alert("Too large")
+    }
+
+
+
+}
+

ad32fd91c070caee4c247e2495fc850776259f2c
Moraes Bruno <moraesbruno888@gmail.com>
2024-08-29 11:45:26 -0300

    adicionado exemplos

---
+++ estudo/function/ex001.js
@@ -0,0 +1,7 @@
+function happyBd(nickName,age) {
+  console.log(`Happy birthday to you`);
+  console.log(`Happy birthday ${nickName}`);
+  console.log(`You are ${age} years old`)
+}
+
+happyBd("John","22");

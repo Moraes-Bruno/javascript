3ca98a30742f5f12ccb86dd5d3307269202e53aa
Bruno Silva <moraesbruno888@gmail.com>
2024-09-03 11:35:19 -0300

    adicionado exemplos

--- estudo/dice_roller/index.js
+++ estudo/dice_roller/index.js
@@ -1,5 +1,18 @@
-const qtdDados = document.getElementById("qtdDados");
-const submit = document.getElementById("submit");
+function rolarDado() {
+  const qtdDados = document.getElementById("qtdDados").value;
+  const resultaDoDado = document.getElementById("resultaDoDado");
+  const dadoImg = document.getElementById("dadoImg");
 
+  const valores = [];
+  const imgs = [];
 
+  for (let i = 0; i < qtdDados; i++) {
+    const valor = Math.floor(Math.random() * 6) + 1;
+    valores.push(valor)
+    imgs.push(`<img src="img/${valor}.png">`)
+  }
 
+  resultaDoDado.textContent = `Dado: ${valores.join(', ')}`
+  dadoImg.innerHTML = imgs.join('');
+  
+}
